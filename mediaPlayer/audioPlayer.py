efe646c277cfb9cc0944ec14a224418906a85d1a
Corey Pi <crydrk@sbcglobal.net>
2020-10-05 19:18:52 +0000

    Audio player successfully playing sounds, responding to verification

--- mediaPlayer/audioPlayer.py
+++ mediaPlayer/audioPlayer.py
@@ -1,18 +1,73 @@
 import pygame
-
-# Initialize the mixer
-pygame.mixer.init()
-
-# Load the audio files
-redAudio = pygame.mixer.Sound("mediaFiles_audio/001_red.wav")
-yellowAudio = pygame.mixer.Sound("mediaFiles_audio/002_yellow.wav")
-blueAudio = pygame.mixer.Sound("mediaFiles_audio/003_blue.wav")
-greenAudio = pygame.mixer.Sound("mediaFiles_audio/004_green.wav")
-
-# Short delay while running processor to allow for loading
-pygame.time.delay(3000)
+import socket
 
 
+audioDict = {}
+
+# UPD setup
+UDP_PORT = 2522
+UDP_IP = ""
+INCOMING_IP = ""
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+
+
+def RunDevice():
+    
+    # Initialize the mixer
+    pygame.mixer.init()
+
+    # Load the audio files
+    redAudio = pygame.mixer.Sound("mediaFiles_audio/001_red.wav")
+    yellowAudio = pygame.mixer.Sound("mediaFiles_audio/002_yellow.wav")
+    blueAudio = pygame.mixer.Sound("mediaFiles_audio/003_blue.wav")
+    greenAudio = pygame.mixer.Sound("mediaFiles_audio/004_green.wav")
+    
+    audioDict["Gryffindor"] = redAudio
+    audioDict["Hufflepuff"] = yellowAudio
+    audioDict["Ravenclaw"] = blueAudio
+    audioDict["Slytherin"] = greenAudio
+
+    # Short delay while running processor to allow for loading
+    pygame.time.delay(3000)
+
+    
+    while True:
+        data, addr = sock.recvfrom(1024)
+        ReceiveData(data, addr)
+
+def ReceiveData(data, addr):
+    print data
+    print addr
+    
+    INCOMING_IP = addr[0]
+    
+    if data == "verify":
+        print "trying to verify"
+        sock.sendto("good", (INCOMING_IP, UDP_PORT))
+        
+    if data in audioDict.keys():
+        # Play the audio if it matches the incoming message
+        pygame.mixer.Sound.play(audioDict[data])
+        
+        while pygame.mixer.Channel(0).get_busy() == True:
+            continue
+            
+        # Workaround to remove any data that came in during audio
+        sock.setblocking(0)
+        while 1:
+            try:
+                throwaway = sock.recvfrom(1024)
+            except:
+                break
+        sock.setblocking(1)
+    
+    
+    
+RunDevice()
+    
 
 # Test plays
 pygame.mixer.Sound.play(redAudio)
