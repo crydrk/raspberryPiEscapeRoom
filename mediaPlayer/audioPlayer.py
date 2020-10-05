3704095e0ee0c683477cfe0804c0d67c33402da4
Corey Pi <crydrk@sbcglobal.net>
2020-10-05 17:53:08 +0000

    Audio files playing in test, ready to add UDP

--- mediaPlayer/audioPlayer.py
+++ mediaPlayer/audioPlayer.py
@@ -1,6 +1,36 @@
 import pygame
+
+# Initialize the mixer
 pygame.mixer.init()
-pygame.mixer.music.load("")
-pygame.mixer.play()
-while pygame.mixer.music.get_busy() == True:
+
+# Load the audio files
+redAudio = pygame.mixer.Sound("mediaFiles_audio/001_red.wav")
+yellowAudio = pygame.mixer.Sound("mediaFiles_audio/002_yellow.wav")
+blueAudio = pygame.mixer.Sound("mediaFiles_audio/003_blue.wav")
+greenAudio = pygame.mixer.Sound("mediaFiles_audio/004_green.wav")
+
+# Short delay while running processor to allow for loading
+pygame.time.delay(3000)
+
+
+
+# Test plays
+pygame.mixer.Sound.play(redAudio)
+
+while pygame.mixer.Channel(0).get_busy() == True:
+    continue
+    
+pygame.mixer.Sound.play(yellowAudio)
+
+while pygame.mixer.Channel(0).get_busy() == True:
+    continue
+    
+pygame.mixer.Sound.play(blueAudio)
+
+while pygame.mixer.Channel(0).get_busy() == True:
+    continue
+
+pygame.mixer.Sound.play(greenAudio)
+
+while pygame.mixer.Channel(0).get_busy() == True:
     continue
