4e2c7ccf1452f35db59b5a145b94ae7f3cdfc2d0
Corey Pi <crydrk@sbcglobal.net>
2020-10-06 17:48:53 +0000

    Video player getting length through os.system and delaying for duration, but this entire method may be unuseable as the shell appears on screen for a few seconds

--- mediaPlayer/videoPlayer.py
+++ mediaPlayer/videoPlayer.py
@@ -2,7 +2,10 @@ import pygame
 import socket
 import os
 
+import subprocess
 from subprocess import Popen
+
+from time import sleep
 
 
 mediaDict = {}
@@ -24,9 +27,42 @@ except:
 
 def RunDevice():
     
-    moviePath = globalFilepath + "/mediaFiles_video/magicShowStart.mp4"
+    dur = subprocess.check_output("avconv -i " + globalFilepath + "/mediaFiles_video/magicShowStart.mp4" + " 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//", shell=True)
     
+    # dur's time format will be: 00:02:21.18
+    
+    minutes = dur.split(':')[1]
+    seconds = dur.split('.')[0].split(':')[2]
+    
+    print "minutes: " + minutes
+    print "seconds: " + seconds
+    
+    milliseconds = int(minutes) * 60 * 1000 + int(seconds) * 1000
+    milliseconds += 1000 # Some extra leeway for loading - this number doesn't need to be spot on
+    
+    print milliseconds / 1000
+    print milliseconds
+    
+    moviePath = globalFilepath + "/mediaFiles_video/magicShowStart.mp4"
     omxp = Popen(['omxplayer', moviePath])
+    
+    print "starting sleep"
+    sleep(milliseconds / 1000)
+    print "ending sleep"
+    
+    moviePath = globalFilepath + "/mediaFiles_video/magicShowStart.mp4"
+    omxp = Popen(['omxplayer', moviePath])
+    
+    #VIDEO_PATH = globalFilepath + "/mediaFiles_video/magicShowStart.mp4"
+    #player = OMXPlayer(VIDEO_PATH)
+    
+    sleep(30)
+    
+    player.quit()
+    
+    #moviePath = globalFilepath + "/mediaFiles_video/magicShowStart.mp4"
+    
+    #omxp = Popen(['omxplayer', moviePath])
 
     """
     while True:
